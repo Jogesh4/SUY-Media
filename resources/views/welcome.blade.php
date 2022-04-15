d32b44c9412a29235b6df843a15845793b17a001
bhanutripathi22 <bhanu@justconsult.us>
2022-04-15 15:56:18 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>Suymedia</title>
+
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/svg-with-js.min.css" integrity="sha512-OiNHhQwzS1LlbvAM+EbRs/LeCL5RhAkv2pvr432TxTFGcxNcG6I8VVII7s4dUVwJJG7HtHKvsR7zzD5vuSlAlA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="aos/aos.css" rel="stylesheet">
+	<link href="{{asset('css/style.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../assets/css/demo.css" />
+   </head>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+	
+	<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-lg-flex d-md-flex text-center align-items-center justify-content-between">
+
+      <div class="logo">
+        <a href="index.html"><img src="../images/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+     <!--- <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>--><!-- .navbar -->
+<div><!--<a class="top-button my-lg-0 my-md-0 my-4"><span><i class="fa fa-phone" aria-hidden="true"></i> +1 234-569-8712</span></a>-->
+<a class="top-button-login my-lg-0 my-md-0 my-4" href="/login"><span><i class="fa fa-user-o" aria-hidden="true"></i> Login</span></a>
+<a class="top-button my-lg-0 my-md-0 my-4" href="/sign-up"><span><i class="fa fa-user-o" aria-hidden="true"></i> Sign Up</span></a>
+</div>
+    </div>
+  </header><!-- End Header -->
+	
+	  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center bg-color">
+
+    <div  class="container ">
+      <div class="row gy-4">
+        <div class="col-lg-5 mt-lg-5 pt-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center z-in">
+          <h1 class="heading">What kind of <span class="orange-text orang-t-size">SEO Service</span></h1>
+          <h2>are you <span class="orange-text">Looking</span> for ?</h2>
+          <!--<div>
+            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+          </div>-->
+		  <div class="call-green">
+<div class="tWLnewIconlogoouter"><div class="TWL-widget_underlay"></div><a href="tel:7548004081"><div class="tWLnewIconlogoinner"><i class="fa fa-phone TWL-widget-button__icon"></i></div></a></div>
+</div>
+        </div>
+        <div class="col-lg-7 order-1 order-lg-2 hero-img z-in">
+          <img src="./21249.svg" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+<div class="ocean">
+  <div class="wave"></div>
+  <div class="wave"></div>
+</div>
+  </section><!-- End Hero -->
+  <section class="py-5">
+   <div  class="container ">
+  <ul class="nav nav-pills mb-3 tab-pro" id="pills-tab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all" aria-selected="true">All</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-seo" type="button" role="tab" aria-controls="pills-seo" aria-selected="false">Managed SEO</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-building" type="button" role="tab" aria-controls="pills-building" aria-selected="false">Link Building</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-Creation" type="button" role="tab" aria-controls="pills-Creation" aria-selected="false">Content Creation</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-traffic" type="button" role="tab" aria-controls="pills-traffic" aria-selected="false">Paid Traffic</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-Local" type="button" role="tab" aria-controls="pills-Local" aria-selected="false">Local</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-analytics" type="button" role="tab" aria-controls="pills-analytics" aria-selected="false">SEO Analytics</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-reputation" type="button" role="tab" aria-controls="pills-reputation" aria-selected="false">Reputation Mgmt.</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-other" type="button" role="tab" aria-controls="pills-other" aria-selected="false">Other</button>
+  </li>
+</ul>
+<div class="tab-content pb-3 pt-3" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab">
+  
+  <div class="row">
+     <div class="col-lg-6 mb-2">
+    <div class="product-item">
+	<div id="ribbon-container">
+      <a href="product-page.html" id="ribbon">Best  Seller</a>
+   </div>
+	<div class="width-40"><img src="./managed-seo.svg" class="img-fluid" alt=""></div>
+	<div class="width-60">
+	<h5>Managed SEO</h5>
+   	 <div class="text-div">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</div>
+	 <a class="text-green pt-3" href="pricing.html">$ Starts at $<span>500</span>/month + $<span>250</span> set up</a>
+	 </div>
+	 </div>
+  </div>
+   <div class="col-lg-6 mb-2">
+    <div class="product-item">
+	<div id="ribbon-container">
+      <a href="product-page.html" id="ribbon">Best  Seller</a>
+   </div>
+	<div class="width-40"><img src="./managed-seo.svg" class="img-fluid" alt=""></div>
+	<div class="width-60">
+	<h5>Managed SEO</h5>
+   	<div class="text-div"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</div>
+	  <a class="text-green pt-3" href="pricing.html">$ Starts at $<span>500</span>/month + $<span>250</span> set up</a>
+	 </div>
+	 </div>
+  </div>
+  </div>
+    <div class="row">
+     <div class="col-lg-6 mb-2">
+    <div class="product-item">
+	<div id="ribbon-container">
+      <a href="product-page.html" id="ribbon">Best  Seller</a>
+   </div>
+	<div class="width-40"><img src="./managed-seo.svg" class="img-fluid" alt=""></div>
+	<div class="width-60">
+	<h5>Managed SEO</h5>
+   	<div class="text-div"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</div>
+	 <a class="text-green pt-3" href="pricing.html">$ Starts at $<span>500</span>/month + $<span>250</span> set up</a>
+	 </div>
+	 </div>
+  </div>
+   <div class="col-lg-6 mb-2">
+    <div class="product-item">
+	<div id="ribbon-container  invisible">
+      <a href="product-page.html" class="invisible" id="ribbon">Best  Seller</a>
+   </div>
+	<div class="width-40"><img src="./managed-seo.svg" class="img-fluid" alt=""></div>
+	<div class="width-60">
+	<h5>Managed SEO</h5>
+   	 <div class="text-div">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</div>
+	 <a class="text-green pt-3" href="pricing.html">$ Starts at $<span>500</span>/month + $<span>250</span> set up</a>
+	 </div>
+	 </div>
+  </div>
+  </div>
+  
+  </div>
+   <div class="tab-pane fade" id="pills-seo" role="tabpanel" aria-labelledby="pills-seo-tab">
+  
+  <div class="row">
+     <div class="col-lg-6">
+    <div class="product-item">
+	<div id="ribbon-container">
+      <a href="product-page.html" id="ribbon">Best  Seller</a>
+   </div>
+	<div class="width-40"><img src="./managed-seo.svg" class="img-fluid" alt=""></div>
+	<div class="width-60">
+	<h5>Managed SEO</h5>
+   	<div class="text-div">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</div>
+	  <a class="text-green pt-3" href="pricing.html">$ Starts at $<span>500</span>/month + $<span>250</span> set up</a>
+	 </div>
+	 </div>
+  </div>
+   <div class="col-lg-6">
+    <div class="product-item">
+	<div id="ribbon-container">
+      <a href="product-page.html" id="ribbon">Best  Seller</a>
+   </div>
+	<div class="width-40"><img src="./managed-seo.svg" class="img-fluid" alt=""></div>
+	<div class="width-60">
+	<h5>Managed SEO</h5>
+   <div class="text-div">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</div>
+	  <a class="text-green pt-3" href="pricing.html">$ Starts at $<span>500</span>/month + $<span>250</span> set up</a>
+	 </div>
+	 </div>
+  </div>
+  </div>
+  
+  </div>
+</div>
+</div>
+  </section>
+  <footer id="footer">
+  <div class="container">
+  <div class="row">
+  <div class="col-lg-8 mb-4">
+  <h2 class="white-text">Let's Start Your Project, <span class="orange-text">Mail Us</span></h2>
+  </div>
+  <div class="col-lg-4 text-lg-end text-md-end mb-4">
+  <h2 class="white-text">+1 123-456-7890</h2>
+  </div> 
+  </div>
+  <div class="row border-top py-5">
+  <div class="col-lg-3">
+  <div class="me-lg-4">
+  <img class="img-fluid py-2" src="../images/logo.png">
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+  </div>
+  </div>
+  <div class="col-lg-3">
+  <div class="me-lg-4">
+  <h4 class="white-text">Services</h4>
+  <ul>
+  <li>Marketing & SEO</li>
+  <li>Startup</li>
+  <li>Finance Solution</li>
+  <li>Food</li>
+  </ul>
+  </div>
+  </div>
+  <div class="col-lg-3">
+  <div class="me-lg-4">
+  <h4 class="white-text">Useful Link</h4>
+  <ul>
+  <li>About</li>
+  <li>Blog</li>
+  <li>Contact</li>
+  <li>Appointment</li>
+  </ul>
+  </div> 
+  </div>
+  <div class="col-lg-3">
+  <div>
+   <h4 class="white-text">Subscribe</h4>
+   <div class="input-group mb-3 mt-4"> <input type="text" class="form-control email-sub" placeholder="Enter email" aria-label="Recipient's username" aria-describedby="button-addon2"> <button class="subscrib" type="button" id="button-addon2">Subscribe</button> </div>
+     Lorem Ipsum is simply dummy text of the printing and typesetting industry.           
+  </div> 
+  </div>  
+  </div>
+  <div class="row pb-3">
+  <div class="col-lg-6">
+  © 2022 Sun Media, All Rights Reserved
+  </div>
+  <div class="col-lg-6 text-lg-end text-md-end">
+ Terms And Conditions | Design By Local SEO
+  </div>
+  </div>
+  </div>
+  </footer>
+	
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+
+  
+<script src="aos/aos.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  
+  </body>
+
+</html>
